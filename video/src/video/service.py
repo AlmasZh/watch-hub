a1eaa91bd1316a8e22a451b7132bc88b7634c7de
AlmasZh <almas.zh22@gmail.com>
2026-02-16 22:44:09 +0500

    feat(video): add endpoint to retrieve single movie by uuid

--- video/src/video/service.py
+++ video/src/video/service.py
@@ -1,11 +1,20 @@
+from fastapi import HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from uuid import UUID
 
 from .models import Movie
 
 
-async def get_movies(db: AsyncSession):
+async def get_movies(db: AsyncSession) -> list[Movie]:
     stmt = select(Movie)
     res = await db.execute(stmt)
     movies = res.scalars().all()
     return movies
+
+async def get_movie_by_uuid(uuid: UUID, db: AsyncSession) -> Movie:
+    movie = await db.get(Movie, uuid)
+
+    if not Movie:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Movie not found")
+    return movie
