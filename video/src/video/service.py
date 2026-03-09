09fe81bf95080a04f826fce060c85746f030a534
AlmasZh <almas.zh22@gmail.com>
2026-03-09 18:18:57 +0500

    feat(video): add get_all_user_videos service function
    Adds a new function to retrieve all videos associated with a specific
    user ID from the database.

--- video/src/video/service.py
+++ video/src/video/service.py
@@ -3,7 +3,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from uuid import UUID
 
-from .models import Movie
+from .models import Movie, UserVideo
 
 
 async def get_movies(db: AsyncSession) -> list[Movie]:
@@ -18,3 +18,9 @@ async def get_movie_by_uuid(uuid: UUID, db: AsyncSession) -> Movie:
     if not Movie:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Movie not found")
     return movie
+
+async def get_all_user_videos(owner_id: int, db: AsyncSession) -> list[UserVideo]:
+    stmt = select(UserVideo).where(UserVideo.owner_id == owner_id)
+    res = await db.execute(stmt)
+    user_videos = res.scalars().all()
+    return user_videos
