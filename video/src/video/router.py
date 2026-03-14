dfee4f9b4c7694805b524acaf9d9e4fed55eae97
AlmasZh <almas.zh22@gmail.com>
2026-03-14 17:33:53 +0500

    feat(video): add delete video endpoint
    Introduces a DELETE route to remove videos. Handles NotFoundError and
    ForbiddenError by raising appropriate FastAPI HTTP exceptions.

--- video/src/video/router.py
+++ video/src/video/router.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from uuid import UUID
 
 from src.database import SessionDep
 from ..auth.dependencies import UserDep
-from .service import get_movies, get_movie_by_uuid, get_all_user_videos
+from .service import get_movies, get_movie_by_uuid, get_all_user_videos, delete_user_video
 from .schemas import MovieResponse, UserVideoResponse
+from .exceptions import ForbiddenError, NotFoundError
 
 
 router = APIRouter(tags=["video"])
@@ -23,3 +24,12 @@ async def get_movie(uuid: UUID, db: SessionDep):
 async def get_user_videos(user: UserDep, db: SessionDep):
     user_videos = await get_all_user_videos(owner_id=user.id, db=db)
     return user_videos
+
+@router.delete('/video/delete/{video_uuid}')
+async def delete_video(video_uuid: UUID, user: UserDep, db: SessionDep):
+    try:
+        await delete_user_video(video_uuid, user, db)
+    except NotFoundError as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+    except ForbiddenError as e:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=str(e))
